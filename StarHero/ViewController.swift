55204b661c22f917659c34aa7b90c2885b5cb09c
fahad rasheed <fahadrasheed@fahads-mac.local>
2020-08-22 04:03:31 -0700

    Initial Commit

---
+++ StarHero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarHero
+//
+//  Created by fahad rasheed on 22/08/2020.
+//  Copyright © 2020 fahad rasheed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
